aaa6ca158d2431aff2715381312969afe4ecc592
Andrew Enfield <aenfield@gmail.com>
2014-11-02 19:18:38 -0800

    Rewrote Monty to use Suite - finally DRY.

--- mymonty.py
+++ mymonty.py
@@ -1,19 +1,7 @@
-from thinkbayes import Pmf
+from thinkbayes import Suite
 
-class Monty(Pmf):
-    
-    def __init__(self, hypos):
-        Pmf.__init__(self)
-        for hypo in hypos:
-            self.Set(hypo, 1)
-        self.Normalize()
-        
-    def Update(self, data):
-        for hypo in self.Values():
-            like = self.Likelihood(data, hypo)
-            self.Mult(hypo, like)
-        self.Normalize()
-        
+class Monty(Suite):
+            
     def Likelihood(self, data, hypo):
         if hypo == data:
             return 0    # Monty will never choose the door with the car, I think
