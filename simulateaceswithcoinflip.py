7ed213849db0c3b65c624b45d785716d91e773ac
Andrew Enfield <aenfield@gmail.com>
2014-11-14 11:44:29 -0800

    Added notes to aces file to simulate #13; copied and renamed old file to show that it was simulating the smoking problem (not aces).

--- simulateaceswithcoinflip.py
+++ simulateaceswithcoinflip.py
@@ -1,20 +1,24 @@
 from random import randint, random
 
-likelihoodSmoker = 0.3
-numberOfStudents = 10000
+numberOfAttempts = 10000
 
 
-def isSmoker():
+def actuallyHaveTwoAcesWhenTheySayTheyHaveOneAce():    
 	if randint(0,1) == 0:
-		# answer truthfully
-		return random() < likelihoodSmoker
+        # pick from the following set, with the specified probabilities
+        # TwoAces: 6/28
+        # OneAceButNotTwo: 14/28
+        # NoAces: 6/28
+
+		# answer truthfully based on what was picked         
+		#return random() < likelihoodOneAce
 	else:
 		# flip another coin and answer based on the result
 		return randint(0,1) == 0
 
 
 def main():
-	smokers = [isSmoker() for i in range(numberOfStudents)]
+	smokers = [isSmoker() for i in range(numberOfAttempts)]
 	print('{:d} said they smoked out of {:d} students ({:.1%}).'.format(sum(smokers), numberOfStudents, sum(smokers)/numberOfStudents))
 
 
